af47711df7a4c545676c6dd6404205dc10286f74
Vitor Gabriel <vitorg.34825@gmail.com>
2022-06-07 11:25:14 -0300

    .env.local

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,9 +3,9 @@ import 'firebase/compat/auth';
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: 'AIzaSyARCRnBv1gTbaDFoQoGxGMJLIHHbVy_3jI',
-        authDomain: 'admin-template-udemy.firebaseapp.com',
-        projectId: 'admin-template-udemy',
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     })
 }
 
